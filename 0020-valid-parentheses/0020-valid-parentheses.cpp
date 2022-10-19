7b76f695758699eebce4690f41fa8280389b57e1
Purushottam Kumar <87223240+PURUSHOTM@users.noreply.github.com>
2022-10-19 21:10:18 +0530

    Time: 2 ms (56.05%), Space: 6.2 MB (80.02%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,27 +1,23 @@
 class Solution
 {
     public:
-        bool isMatch(char ch1, char ch2)
+        bool isValid(string s)
         {
-            if (ch1 == '(' && ch2 == ')') return true;
-            if (ch1 == '[' && ch2 == ']') return true;
-            if (ch1 == '{' && ch2 == '}') return true;
-            return false;
+            stack<char> st;	//taking stack for keep tracking the order of the brackets..
+            for (auto i: s)	//iterate over each and every elements
+            {
+                if (i == '('
+                    or i == '{'
+                    or i == '[') st.push(i);	//if current element of the string will be opening bracket then we will just simply push it into the stack
+                else	//if control comes to else part, it means that current element is a closing bracket, so check two conditions  current element matches with top of the stack and the stack must not be empty...
+                {
+                    if (st.empty() or(st.top() == '('
+                            and i != ')') or(st.top() == '{'
+                            and i != '}') or(st.top() == '['
+                            and i != ']')) return false;
+                    st.pop();	//if control reaches to that line, it means we have got the right pair of brackets, so just pop it.
+                }
+            }
+            return st.empty();	//at last, it may possible that we left something into the stack unpair so return checking stack is empty or not..
         }
-    bool isValid(string s) {
-         int top = -1;
-    for(int i =0;i<s.length();i++){
-        //If top <0 or the current character is not matched
-        //with the with open bracket at the top index
-        if(top<0 || !isMatch(s[top], s[i])){
-            ++top;
-            s[top] = s[i];
-        }
-        //current character is matched with the with open bracket at the top index
-        else{
-            --top;
-        }
-    }
-    return top == -1;
-    }
 };

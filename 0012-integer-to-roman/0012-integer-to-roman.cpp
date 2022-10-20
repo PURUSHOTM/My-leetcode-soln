6308ea7d5688350b703925f832721410a871fce7
Purushottam Kumar <87223240+PURUSHOTM@users.noreply.github.com>
2022-10-20 12:06:31 +0530

    Time: 15 ms (55.72%), Space: 5.9 MB (77.21%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -0,0 +1,36 @@
+// class Solution {
+// public:
+//     const int val[13] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+//     const string rom[13] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+//     string intToRoman(int N) {
+//         string ans = "";
+//         for (int i = 0; N; i++)
+//             while (N >= val[i]) ans += rom[i], N -= val[i];
+//         return ans;
+//     }
+// };
+
+
+
+
+
+class Solution {
+public:
+const int val[13] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+const string rom[13] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+string intToRoman(int N) {
+    string ans = "";
+    int i = 0;
+    while(N){
+      while (N >= val[i]){
+         ans += rom[i];
+         N -= val[i];
+        }
+        i++;
+    }
+    
+    return ans;
+}
+};

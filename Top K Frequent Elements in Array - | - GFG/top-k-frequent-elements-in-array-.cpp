c36dbb2782b16ba89ec457c96e7662659f6f3326
Purushottam Kumar <87223240+PURUSHOTM@users.noreply.github.com>
2023-06-30 16:42:54 +0530

    Added solution - LeetHub

---
+++ Top K Frequent Elements in Array - | - GFG/top-k-frequent-elements-in-array-.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> topK(vector<int>& nums, int k) { 
+        unordered_map<int,int> mp;
+        for(int i = 0; i<nums.size(); i++){
+            mp[nums[i]]++;
+        }
+        
+        priority_queue<pair<int , int> , vector<pair<int , int>> , greater<pair<int, int>>> pq;
+        for (auto it : mp) {
+             pq.push({it.second, it.first});
+            if (pq.size() > k) pq.pop();
+        }
+        
+        vector<int> ans;
+        while(pq.size() > 0){ 
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n;
+        cin >> n;
+        vector<int> nums(n);
+        for (auto &i : nums) cin >> i;
+        int k;
+        cin >> k;
+        Solution obj;
+        vector<int> ans = obj.topK(nums, k);
+        for (auto i : ans) cout << i << " ";
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends

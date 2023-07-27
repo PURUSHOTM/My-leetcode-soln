c929d9f020b62d42b73da99a486517aa1a6c7713
Purushottam Kumar <87223240+PURUSHOTM@users.noreply.github.com>
2023-07-27 12:05:10 +0530

    Added solution - LeetHub

--- Chocolate Distribution Problem - GFG/chocolate-distribution-problem.cpp
+++ Chocolate Distribution Problem - GFG/chocolate-distribution-problem.cpp
@@ -11,7 +11,7 @@ class Solution{
         int i = 0;
         int j = m-1;
         int mini = INT_MAX;
-        while( j < a.size()){
+        while( j < n){
             int diff = a[j] - a[i];
             mini = min(mini , diff);
             i++;
